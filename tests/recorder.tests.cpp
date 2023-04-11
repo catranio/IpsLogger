80f0654f35d59aded36132d339c3b027dbd95fba
Petr Iaskevich <iaskdeveloper@gmail.com>
2023-04-11 23:17:39 +0300

    fix:  - rewrite recorder tests to fixture  - added string and string_view tests

--- tests/recorder.tests.cpp
+++ tests/recorder.tests.cpp
@@ -3,12 +3,65 @@
 #include <limits>
 #include <sstream>
 
-TEST_CASE("correct constructor") {
-	auto severity = ips::logger::Severity::DEBUG;
-	auto level = ips::logger::level_t{6};
-	auto loggerId = ips::logger::id_t{233};
+using namespace ::ips::logger;
 
-	auto rec = ips::logger::Recorder{severity, level, loggerId};
+class RecorderFixture {
+public:
+    RecorderFixture() : recorder(severity, level, loggerId) {};
+
+    template<typename T>
+    void check_value(const T& value, const std::string& expected) {
+        recorder << value;
+        CHECK(recorder.getBuffer() == expected);
+    }
+
+    template<typename TYPE>
+    void check_numeric_value() {
+        SUBCASE("min") {
+            auto value = std::numeric_limits<TYPE>::min();
+            check_value(value, std::to_string(value));
+        }
+        SUBCASE("max") {
+            auto value = std::numeric_limits<TYPE>::max();
+            check_value(value, std::to_string(value));
+        }
+        if constexpr (std::is_integral_v<TYPE>) {
+            SUBCASE("random") {
+                auto value = std::numeric_limits<TYPE>::max() % static_cast<TYPE>(rand());
+                check_value(value, std::to_string(value));
+            }
+        }
+    }
+
+    template<typename TYPE>
+    void check_double_value() {
+        SUBCASE("min") {
+            auto value = static_cast<TYPE>(-42.1);
+            check_value(value, "-42.1");
+        }
+        SUBCASE("max") {
+            auto value = static_cast<TYPE>(42.1);
+            check_value(value, "42.1");
+        }
+
+        SUBCASE("zero") {
+            auto value = static_cast<TYPE>(0);
+            check_value(value, "0");
+        }
+    }
+
+    static constexpr Severity severity = Severity::DEBUG;
+    static constexpr level_t level = level_t{6};
+    static constexpr id_t loggerId = id_t{233};
+    Recorder recorder;
+};
+
+TEST_CASE("correct constructor") {
+	auto severity = Severity::DEBUG;
+	auto level = level_t{6};
+	auto loggerId = id_t{233};
+
+	auto rec = Recorder{severity, level, loggerId};
 
 	CHECK(rec.getSeverity() == severity);
 	CHECK(rec.getLevel() == level);
@@ -16,112 +69,80 @@ TEST_CASE("correct constructor") {
 	CHECK(rec.getBuffer().empty());
 }
 
-template<typename T>
-void check_value(const T& value, const std::string& expected) {
-	auto rec = ips::logger::Recorder{ips::logger::Severity::ALL, 3, 0};
-	rec << value;
-	CHECK(rec.getBuffer() == expected);
-}
+TEST_CASE_FIXTURE(RecorderFixture, "format") {
+    SUBCASE("unsigned") {
+        check_numeric_value<unsigned>();
+    }
 
-template<typename TYPE>
-void check_numeric_value() {
-	SUBCASE("min") {
-		auto value = std::numeric_limits<TYPE>::min();
-		check_value(value, std::to_string(value));
-	}
-	SUBCASE("max") {
-		auto value = std::numeric_limits<TYPE>::max();
-		check_value(value, std::to_string(value));
-	}
-	if constexpr (std::is_integral_v<TYPE>) {
-		SUBCASE("random") {
-			auto value = std::numeric_limits<TYPE>::max() % static_cast<TYPE>(rand());
-			check_value(value, std::to_string(value));
-		}
-	}
-}
+    SUBCASE("unsigned long") {
+        check_numeric_value<unsigned long>();
+    }
 
-template<typename TYPE>
-void check_double_value() {
-	SUBCASE("min") {
-		auto value = static_cast<TYPE>(-42.1);
-		check_value(value, "-42.1");
-	}
-	SUBCASE("max") {
-		auto value = static_cast<TYPE>(42.1);
-		check_value(value, "42.1");
-	}
+    SUBCASE("unsigned long long") {
+        check_numeric_value<unsigned long long>();
+    }
 
-	SUBCASE("zero") {
-		auto value = static_cast<TYPE>(0);
-		check_value(value, "0");
-	}
-}
+    SUBCASE("int") {
+        check_numeric_value<int>();
+    }
 
-TEST_CASE("") {
-	SUBCASE("unsigned") {
-		check_numeric_value<unsigned>();
-	}
+    SUBCASE("long") {
+        check_numeric_value<long>();
+    }
 
-	SUBCASE("unsigned long") {
-		check_numeric_value<unsigned long>();
-	}
+    SUBCASE("long long") {
+        check_numeric_value<long long>();
+    }
 
-	SUBCASE("unsigned long long") {
-		check_numeric_value<unsigned long long>();
-	}
+    SUBCASE("double") {
+        check_double_value<double>();
+    }
 
-	SUBCASE("int") {
-		check_numeric_value<int>();
-	}
+    SUBCASE("long double") {
+        check_double_value<long double>();
+    }
 
-	SUBCASE("long") {
-		check_numeric_value<long>();
-	}
+    SUBCASE("float") {
+        check_double_value<float>();
+    }
 
-	SUBCASE("long long") {
-		check_numeric_value<long long>();
-	}
+    SUBCASE("char") {
+        SUBCASE("min") {
+            auto value = std::numeric_limits<char>::min();
+            check_value(value, std::string{value});
+        }
+        SUBCASE("max") {
+            auto value = std::numeric_limits<char>::max();
+            check_value(value, std::string{value});
+        }
+    }
 
-	SUBCASE("double") {
-		check_double_value<double>();
-	}
+    SUBCASE("bool") {
+        SUBCASE("true") {
+            check_value(true, "true");
+        }
+        SUBCASE("false") {
+            check_value(false, "false");
+        }
+    }
 
-	SUBCASE("long double") {
-		check_double_value<long double>();
-	}
+    SUBCASE("exception") {
+        try {
+            throw std::runtime_error("runtime error");
+        } catch (std::exception& e) {
+            recorder << e;
+        }
+        CHECK(recorder.getBuffer() == "runtime error");
+    }
 
-	SUBCASE("float") {
-		check_double_value<float>();
-	}
+    SUBCASE("string") {
+        auto value = std::string{"foo"};
+        check_value(value, value);
+    }
 
-	SUBCASE("char") {
-		SUBCASE("min") {
-			auto value = std::numeric_limits<char>::min();
-			check_value(value, std::string{value});
-		}
-		SUBCASE("max") {
-			auto value = std::numeric_limits<char>::max();
-			check_value(value, std::string{value});
-		}
-	}
-
-	SUBCASE("bool") {
-		SUBCASE("true") {
-			check_value(true, "true");
-		}
-		SUBCASE("false") {
-			check_value(false, "false");
-		}
-	}
-
-	SUBCASE("exception") {
-		auto rec = ips::logger::Recorder{ips::logger::Severity::ALL, 3, 0};
-		try {
-			throw std::runtime_error("runtime error");
-		} catch (std::exception& e) {
-			rec << e.what();
-		}
-		CHECK(rec.getBuffer() == "runtime error");
-	}
+    SUBCASE("string_view") {
+        auto value = std::string_view{"baz"};
+        recorder << value;
+        CHECK(recorder.getBuffer() == value);
+    }
 }
