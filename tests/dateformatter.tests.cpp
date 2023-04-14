973706f32aff875e38803215f6f25d4cfd86851d
Petr Iaskevich <iaskdeveloper@gmail.com>
2023-04-14 03:35:04 +0300

    chore: add tests for DateFormatter and StorageLoggers

---
+++ tests/dateformatter.tests.cpp
@@ -0,0 +1,49 @@
+#include <doctest/doctest.h>
+#include <ips/logger/recorder.hpp>
+#include <ips/logger/definitions.hpp>
+#include <details/dateFormatter.hpp>
+#include <fmt/format.h>
+#include <fmt/chrono.h>
+
+
+using namespace ::ips::logger;
+
+TEST_SUITE_BEGIN("formatter");
+
+constexpr std::string_view to_string(Severity severity) noexcept {
+    switch (severity) {
+        case Severity::FATAL: return "fatal";
+        case Severity::ERROR: return "error";
+        case Severity::WARNING: return "warning";
+        case Severity::INFO: return "info";
+        case Severity::TRACE: return "trace";
+        case Severity::DEBUG: return "debug";
+        case Severity::ALL: return "all";
+        case Severity::NONE:
+        default: return "";
+    }
+}
+
+TEST_CASE("dateformatter") {
+    const auto severity = Severity::TRACE;
+    const auto level = 2;
+    const auto id = 0;
+    const auto message = "some message for logging.";
+
+    details::DateFormatter df;
+    Recorder rec{severity, level, id};
+    rec << message;
+    auto assert = df.fmt(rec);
+
+    std::chrono::microseconds mc(rec.getTimestamp());
+    auto tp = std::chrono::time_point<std::chrono::system_clock>(mc);
+    auto expect = fmt::format("[{:%Y.%m.%d %H:%M:}{:%S}] {}({}): {}\n",
+                       tp,
+                       mc,
+                       ips::logger::to_string(rec.getSeverity()),
+                       rec.getLevel(),
+                       rec.getBuffer());
+    CHECK(assert == expect);
+}
+
+TEST_SUITE_END();
