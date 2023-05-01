1ae3fa838d08e03a9edffefb6d94c60fb011469b
Petr Iaskevich <iaskdeveloper@gmail.com>
2023-05-02 00:24:00 +0300

    fix: print time in linux

--- sources/details/dateFormatter.cpp
+++ sources/details/dateFormatter.cpp
@@ -8,10 +8,16 @@
 using namespace ips::logger::details;
 
 std::string DateFormatter::fmt(const Recorder &recorder) const noexcept {
-	std::chrono::microseconds mc(recorder.getTimestamp());
-	auto tp = std::chrono::time_point<std::chrono::system_clock>(mc);
+    auto timestamp = recorder.getTimestamp();
+#if defined(__APPLE__)
+    timestamp *= 1000; /* linux use nanoseconds for time_point*/
+#endif
+    auto ns = std::chrono::nanoseconds{timestamp};
+    auto mc = std::chrono::duration_cast<std::chrono::microseconds>(ns);
+    auto sec = std::chrono::duration_cast<std::chrono::seconds>(ns);
+    auto tp = std::chrono::time_point<std::chrono::system_clock>(mc);
     return fmt::format("[{:%Y.%m.%d %H:%M:}{:%S}] {}({}): {}\n",
-					   tp, mc,
+                       tp, mc - sec,
                        ips::logger::to_string(recorder.getSeverity()),
                        recorder.getLevel(),
                        recorder.getBuffer());
