3839a465900be39ad31b936ca39024ad57887f35
Petr Iaskevich <iaskdeveloper@gmail.com>
2023-05-05 02:06:28 +0300

    fix: print time in microseconds

--- sources/details/dateFormatter.cpp
+++ sources/details/dateFormatter.cpp
@@ -12,12 +12,11 @@ std::string DateFormatter::fmt(const Recorder &recorder) const noexcept {
 #if defined(__APPLE__)
     timestamp *= 1000; /* linux use nanoseconds for time_point*/
 #endif
-    auto ns = std::chrono::nanoseconds{timestamp};
-    auto mc = std::chrono::duration_cast<std::chrono::microseconds>(ns);
-    auto sec = std::chrono::duration_cast<std::chrono::seconds>(ns);
-    auto tp = std::chrono::time_point<std::chrono::system_clock>(mc);
+    const auto &ns = std::chrono::nanoseconds{timestamp};
+    const auto &mc = std::chrono::duration_cast<std::chrono::microseconds>(ns);
+    const auto &tp = std::chrono::time_point<std::chrono::system_clock>(mc);
     return fmt::format("[{:%Y.%m.%d %H:%M:}{:%S}] {}({}): {}\n",
-                       tp, mc - sec,
+                       tp, mc,
                        ips::logger::to_string(recorder.getSeverity()),
                        recorder.getLevel(),
                        recorder.getBuffer());
