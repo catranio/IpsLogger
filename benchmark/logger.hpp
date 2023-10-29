27cb7c977daa0d8352ef503ebcfa809fe1a11d87
Petr Iaskevich <iaskdeveloper@gmail.com>
2023-10-29 16:43:24 +0300

    chore: add simple benchmarking with spdlog

--- benchmark/logger.hpp
+++ benchmark/logger.hpp
@@ -2,18 +2,53 @@
 #define IPSLOGGER_LOGGER_HPP
 
 #include <benchmark/benchmark.h>
+#include <spdlog/async.h>
+#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/spdlog.h>
 
+#include <fstream>
 #include <iostream>
 #include <ips/logger/log.hpp>
 
 namespace logger {
-[[maybe_unused]] static void one_log(benchmark::State& state) {
-  using enum ips::logger::Severity;
-  ipslog::init::console(kAll);
+[[maybe_unused]] static void ipslog_console(benchmark::State& state) {
+  if (state.thread_index() == 0) {
+    ipslog::init::console(ips::logger::Severity::kAll);
+  }
   for ([[maybe_unused]] auto _ : state) {
     ipslog::info() << "some logger string #" << 1;
   }
 }
+
+[[maybe_unused]] static void ipslog_file(benchmark::State& state) {
+  if (state.thread_index() == 0) {
+    ipslog::init::file("trace", "ipslog.log", ips::logger::Severity::kAll);
+  }
+  for ([[maybe_unused]] auto _ : state) {
+    ipslog::info() << "some logger string #" << 1;
+  }
+}
+
+// spdlog
+[[maybe_unused]] static void spdlog_console(benchmark::State& state) {
+  if (!spdlog::get("console") && state.thread_index() == 0) {
+    spdlog::stdout_color_mt("console");
+  }
+  for ([[maybe_unused]] auto _ : state) {
+    spdlog::get("console")->info("some logger string #{}", 1);
+  }
+}
+
+[[maybe_unused]] static void spdlog_file(benchmark::State& state) {
+  if (!spdlog::get("basic_logger") && state.thread_index() == 0) {
+    spdlog::basic_logger_mt("basic_logger", "spdlog.log");
+  }
+  for ([[maybe_unused]] auto _ : state) {
+    spdlog::get("basic_logger")->info("some logger string #{}", 1);
+  }
+}
+
 }  // namespace logger
 
 #endif /* IPSLOGGER_LOGGER_HPP */
--- benchmark/main.banchmark.cpp
+++ benchmark/main.banchmark.cpp
@@ -3,7 +3,37 @@
 #include "logger.hpp"
 
 int main(int argc, char** argv) {
-  benchmark::RegisterBenchmark("logger", logger::one_log);
+  // ipslog console
+  //  benchmark::RegisterBenchmark("console/ipslog", logger::ipslog_console);
+  //  benchmark::RegisterBenchmark("console/ipslog", logger::ipslog_console)
+  //      ->Threads(2);
+  //  benchmark::RegisterBenchmark("console/ipslog", logger::ipslog_console)
+  //      ->Threads(4);
+  //  benchmark::RegisterBenchmark("console/ipslog", logger::ipslog_console)
+  //      ->Threads(8);
+  //
+  //  // spdlog console
+  //  benchmark::RegisterBenchmark("console/spdlog", logger::spdlog_console);
+  //  benchmark::RegisterBenchmark("console/spdlog", logger::spdlog_console)
+  //      ->Threads(2);
+  //  benchmark::RegisterBenchmark("console/spdlog", logger::spdlog_console)
+  //      ->Threads(4);
+  //  benchmark::RegisterBenchmark("console/spdlog", logger::spdlog_console)
+  //      ->Threads(8);
+
+  // ipslog file
+  benchmark::RegisterBenchmark("file/ipslog", logger::ipslog_file);
+  benchmark::RegisterBenchmark("file/ipslog", logger::ipslog_file)->Threads(2);
+  benchmark::RegisterBenchmark("file/ipslog", logger::ipslog_file)->Threads(4);
+  benchmark::RegisterBenchmark("file/ipslog", logger::ipslog_file)->Threads(8);
+
+  // spdlog file
+  benchmark::RegisterBenchmark("file/spdlog", logger::spdlog_file);
+  benchmark::RegisterBenchmark("file/spdlog", logger::spdlog_file)->Threads(2);
+  benchmark::RegisterBenchmark("file/spdlog", logger::spdlog_file)->Threads(4);
+  benchmark::RegisterBenchmark("file/spdlog", logger::spdlog_file)->Threads(8);
+
+  // initialize
   benchmark::Initialize(&argc, argv);
   benchmark::RunSpecifiedBenchmarks();
   benchmark::Shutdown();
